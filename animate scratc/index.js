acaf12d9ebd108604439715d804de4614cfe198a
sansrien <63850220+sansrien@users.noreply.github.com>
2022-03-07 18:57:46 +0800

    loader

---
+++ animate scratc/index.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add('square-animation');
+    }
+  });
+});
+
+observer.observe(document.querySelector('.square'));
--- index.js
+++ index.js
@@ -1,41 +1,29 @@
-// console.log('here');
+document.onreadystatechange = function() {
+  if (document.readyState !== "complete") {
+      document.querySelector(
+        "body").style.visibility = "hidden";
+      document.querySelector(
+        "#loader").style.visibility = "visible";
+  } else {
+      document.querySelector(
+        "#loader").style.display = "none";
+      document.querySelector(
+        "body").style.visibility = "visible";
+  }
+};
 
-// const navbar = document.getElementById('Header');
-// const navLogo = document. querySelector('#header-img')
-// window.onscroll = () => {
-//     if (window.scrollY > 300) {
-//         navbar.classList.add('nav-active');
-//         navLogo.classList.add('img-active');
-//     } else {
-//         console.log(navbar)
-//         navbar.classList.remove('nav-active');
-//         navLogo.classList.remove('img-active');
-//     }
-// };
-// var top1 = $('#about-me').offset().top;
-// var top2 = $('#skills').offset().top;
-// var top3 = $('#project').offset().top;
-
-
-// $(document).scroll(function(){
-//     var scrollPos = $(document).scrollTop();
-//     if (scrollPos >= top1 && scrollPos < top2) {
-//         $('#navbar').css('background-color', '#f00');
-//       } else if (scrollPos >= top2 && scrollPos < top3) {
-//         $('#navbar').css('background-color', '#0f0');
-//       } else if (scrollPos >= top3) {
-//         $('#navbar').css('background-color', '#00f');
-//       }
-// });
 var top0 = $('#welcome-section').offset().top;
 var top1 = $('#about-me').offset().top;
 var top2 = $('#skills').offset().top;
 var top3 = $('#projects').offset().top;
 
 $(document).ready(function(){
-  if($(window).width() < 550 || $(document).width() < 550  ){
-    $('#nav-links').hide();
-  }
+  // if($(window).width() < 550 || $(document).width() < 550  ){
+  //   $('#nav-links').hide();
+  // }else{
+  //   $('#nav-links').unhide();
+  // }
+
   $(document).scroll(function() {
     var scrollPos = $(document).scrollTop();
     if (scrollPos >= top0 && scrollPos < top1){
