b83dd5d2c3c0b181836f5826f56d30ceb7ce15c8
sansrien <63850220+sansrien@users.noreply.github.com>
2022-03-07 15:52:37 +0800

    hide trial

--- index.js
+++ index.js
@@ -32,18 +32,23 @@ var top1 = $('#about-me').offset().top;
 var top2 = $('#skills').offset().top;
 var top3 = $('#projects').offset().top;
 
-$(document).scroll(function() {
-  var scrollPos = $(document).scrollTop();
-  if (scrollPos >= top0 && scrollPos < top1){
-    $('#change a').css('color', 'black');
-    $('nav img').css('filter', 'none');
-  }else if (scrollPos >= top1 && scrollPos < top2) {
-    $('#change a').css('color', 'white');
-    $('nav img').css('filter', 'invert(100) sepia(100)');
-  } else if (scrollPos >= top2 && scrollPos < top3) {
-    $('#change a').css('color', 'white');
-    $('nav img').css('filter', 'none');
-  } else if (scrollPos >= top3) {
-    $('#change a').css('color', '#53151f');
+$(document).ready(function(){
+  if($(window).width() < 550){
+    $('nav').hide();
   }
+  $(document).scroll(function() {
+    var scrollPos = $(document).scrollTop();
+    if (scrollPos >= top0 && scrollPos < top1){
+      $('#change a').css('color', 'black');
+      $('nav img').css('filter', 'none');
+    }else if (scrollPos >= top1 && scrollPos < top2) {
+      $('#change a').css('color', 'white');
+      $('nav img').css('filter', 'invert(100) sepia(100)');
+    } else if (scrollPos >= top2 && scrollPos < top3) {
+      $('#change a').css('color', 'white');
+      $('nav img').css('filter', 'none');
+    } else if (scrollPos >= top3) {
+      $('#change a').css('color', '#53151f');
+    }
+  });
 });
