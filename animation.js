60866a9a37dad9e021aee0d6f16a5c057b960892
sansrien <63850220+sansrien@users.noreply.github.com>
2022-03-08 21:44:24 +0800

    animation trial

--- animation.js
+++ animation.js
@@ -13,3 +13,43 @@ var animateMe = new Vue({
         }
     }
 });
+
+// var animateMe = new Vue({
+//     el: '#demo',
+//     data: {
+//         showAnimation: false,
+//     },
+//     methods: {
+//         isViewableNow(isVisible, entry) {
+//             this.showAnimation = isVisible;
+//         }
+//     }
+// });
+
+var animateMe2 = new Vue({
+    el: '#about-me',
+    data: {
+        showAnimationFor: {
+            a: false,
+            b: false,
+            c: false,
+        }
+    },
+    methods: {
+        isViewableNow(isVisible, entry, section) {
+            this.showAnimationFor[section] = isVisible;
+        }
+    }
+});
+
+var animateMe3  = new Vue({
+    el: '#skills',
+    data: {
+        showAnimation: false,
+    },
+    methods: {
+        isViewableNow(isVisible, entry) {
+            this.showAnimation = isVisible;
+        }
+    }
+});
