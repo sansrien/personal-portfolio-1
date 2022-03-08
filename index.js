0429c0e5b75ba51dac8316ca729db670734cb852
sansrien <63850220+sansrien@users.noreply.github.com>
2022-03-08 22:08:48 +0800

    nav fix

--- index.js
+++ index.js
@@ -10,20 +10,27 @@ $(document).ready(function(){
 
   $(document).scroll(function() {
     var scrollPos = $(document).scrollTop();
-    if (scrollPos >= top0 && scrollPos < top1){
+    if(scrollPos <top0){
       $('#change a').css('color', 'black');
       $('nav img').css('filter', 'none');
-    }else if (scrollPos >= top1 && scrollPos < top2) {
+      console.log("viewing welcome")
+    }else if (scrollPos >= top0 && scrollPos < top1){
       $('#change a').css('color', 'white');
       $('nav img').css('filter', 'invert(100) sepia(100)');
-    } else if (scrollPos >= top2 && scrollPos < top3) {
+
+      console.log("viewing welcome")
+    }else if (scrollPos >= top1 && scrollPos < top2) {
       $('#change a').css('color', 'white');
       $('nav img').css('filter', 'none');
+      console.log("viewing about-me")
+    } else if (scrollPos >= top2 && scrollPos < top3) {
+      $('#change a').css('color', '#53151f');
+      $('nav img').css('filter', 'none');
     } else if (scrollPos >= top3 && scrollPos < top4) {
-        $('#change a').css('color', '#53151f');
+        $('#change a').css('color', 'rgb(194, 126, 42)');
         $('nav img').css('filter', 'none');
      }else if (scrollPos >= top4) {
-        $('#change a').css('color', 'white');
+        $('#change a').css('color', 'rgb(194, 126, 42)');
         $('nav img').css('filter', 'invert(100) sepia(100)');
     }
   });
